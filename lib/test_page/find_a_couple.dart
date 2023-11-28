d079a671f0da3a4a24b20f256660276f8fd14829
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-11-28 18:19:26 +0700

    logic

--- lib/test_page/find_a_couple.dart
+++ lib/test_page/find_a_couple.dart
@@ -8,6 +8,18 @@ class FindACouplePage extends StatefulWidget {
 }
 
 class _FindACouplePageState extends State<FindACouplePage> {
+  List<Word> words = [];
+  @override
+  void initState() {
+    super.initState();
+    words.add(Word(arabic: 'مرحبًا', kyrgyz: 'мерхаба'));
+    words.add(Word(arabic: 'شراء', kyrgyz: 'салах'));
+    words.add(Word(arabic: 'القرآن', kyrgyz: 'куран'));
+    words.add(Word(arabic: 'مرحبًا', kyrgyz: 'мерхаба'));
+    words.add(Word(arabic: 'مرحبًا', kyrgyz: 'мерхаба'));
+    words.add(Word(arabic: 'مرحبًا', kyrgyz: 'мерхаба'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,7 +91,7 @@ class _FindACouplePageState extends State<FindACouplePage> {
                     Icon(
                       Icons.favorite_border_outlined,
                       size: 24,
-                      color: Colors.red,
+                      color: Colors.grey,
                     ),
                     Icon(
                       Icons.favorite_border_outlined,
@@ -107,17 +119,18 @@ class _FindACouplePageState extends State<FindACouplePage> {
               child: GridView.builder(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
-                itemCount: 10,
+                itemCount: words.length,
                 gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                     maxCrossAxisExtent: 220,
                     crossAxisSpacing: 20,
                     childAspectRatio: 3.5 / 2),
                 itemBuilder: (contex, index) {
+                  var word = words[index];
                   return GestureDetector(
                     onTap: () {},
-                    child: const Card(
+                    child: Card(
                       color: Colors.white,
-                      shape: RoundedRectangleBorder(
+                      shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(
                           Radius.circular(10),
                         ),
@@ -125,18 +138,18 @@ class _FindACouplePageState extends State<FindACouplePage> {
                           color: Color(0xFF236681),
                         ),
                       ),
-                      margin: EdgeInsets.symmetric(vertical: 10),
+                      margin: const EdgeInsets.symmetric(vertical: 10),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            'Мархаба',
-                            style: TextStyle(
+                            word.arabic,
+                            style: const TextStyle(
                               color: Color(0xFF236681),
                               fontSize: 17,
                               fontWeight: FontWeight.w400,
                             ),
-                          )
+                          ),
                         ],
                       ),
                     ),
@@ -149,4 +162,14 @@ class _FindACouplePageState extends State<FindACouplePage> {
       ),
     );
   }
+}
+
+class Word {
+  final String arabic;
+  final String kyrgyz;
+
+  Word({
+    required this.arabic,
+    required this.kyrgyz,
+  });
 }
